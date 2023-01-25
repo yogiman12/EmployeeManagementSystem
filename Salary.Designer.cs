93c1ca277180f406c1d41156bf5a3e80a288bdc6
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-25 17:41:24 +0200

    edit the start position in new window

--- Salary.Designer.cs
+++ Salary.Designer.cs
@@ -29,9 +29,18 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Salary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Salary";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Salary";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
