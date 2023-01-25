f39d2306c1e847552b35769e8f1365fa5af2f54a
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-25 17:14:32 +0200

    edit button

--- .Designer.cs
+++ .Designer.cs
@@ -269,13 +269,14 @@ namespace E.M.S
             this.button1.BackColor = System.Drawing.SystemColors.HighlightText;
             this.button1.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.button1.Location = new System.Drawing.Point(642, 504);
             this.button1.Location = new System.Drawing.Point(87, 998);
+            this.button1.Location = new System.Drawing.Point(44, 989);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(129, 47);
             this.button1.TabIndex = 23;
             this.button1.Text = "Login";
+            this.button1.Text = "add";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // Form1
