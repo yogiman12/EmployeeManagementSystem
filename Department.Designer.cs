e5cf9415f3d6168e42c95afeda0330e431fa4a28
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-25 17:38:18 +0200

    add another txtbox and name it as Department List

--- Department.Designer.cs
+++ Department.Designer.cs
@@ -37,6 +37,7 @@ namespace E.M.S
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -47,6 +48,7 @@ namespace E.M.S
             this.button1.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.Highlight;
             this.button1.Location = new System.Drawing.Point(47, 1003);
+            this.button1.Location = new System.Drawing.Point(62, 525);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(101, 47);
@@ -57,7 +59,6 @@ namespace E.M.S
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(78, 191);
             this.pictureBox1.Location = new System.Drawing.Point(78, 227);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(141, 98);
@@ -73,10 +74,8 @@ namespace E.M.S
             this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
             this.label3.Location = new System.Drawing.Point(42, 354);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(177, 28);
             this.label3.Size = new System.Drawing.Size(201, 28);
             this.label3.TabIndex = 29;
-            this.label3.Text = "Employee Name";
             this.label3.Text = "Department Name";
             // 
             // textBox1
@@ -125,6 +124,7 @@ namespace E.M.S
             this.button2.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.Highlight;
             this.button2.Location = new System.Drawing.Point(183, 1003);
+            this.button2.Location = new System.Drawing.Point(198, 525);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(92, 47);
@@ -132,12 +132,25 @@ namespace E.M.S
             this.button2.Text = "Update";
             this.button2.UseVisualStyleBackColor = false;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(606, 513);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 28);
+            this.label4.TabIndex = 43;
+            this.label4.Text = "Department List";
+            // 
             // Department
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(1412, 1055);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
@@ -169,5 +182,6 @@ namespace E.M.S
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
     }
 }
