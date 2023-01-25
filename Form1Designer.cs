6c655672debc8763e0797399890f43220eaed1f6
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-25 16:43:46 +0200

    another customize

--- Form1Designer.cs
+++ Form1Designer.cs
@@ -60,8 +60,8 @@ namespace E.M.S
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Trebuchet MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label2.Location = new System.Drawing.Point(627, 213);
             this.label2.Location = new System.Drawing.Point(564, 220);
+            this.label2.Location = new System.Drawing.Point(577, 222);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(239, 36);
             this.label2.TabIndex = 1;
