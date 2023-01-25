c786b2f647d020b0f77404a2bf1ea231c48c263a
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-25 16:44:56 +0200

    add another label

--- Form1Designer.cs
+++ Form1Designer.cs
@@ -33,6 +33,8 @@ namespace E.M.S
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -76,10 +78,8 @@ namespace E.M.S
             this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
             this.label3.Location = new System.Drawing.Point(51, 333);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(124, 28);
             this.label3.Size = new System.Drawing.Size(177, 28);
             this.label3.TabIndex = 7;
-            this.label3.Text = "User Name";
             this.label3.Text = "Employee Name";
             // 
             // textBox1
@@ -89,11 +89,32 @@ namespace E.M.S
             this.textBox1.Size = new System.Drawing.Size(292, 34);
             this.textBox1.TabIndex = 6;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(51, 448);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 28);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Employee Name";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(54, 493);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(292, 34);
+            this.textBox2.TabIndex = 8;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1412, 850);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
@@ -117,5 +138,7 @@ namespace E.M.S
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
