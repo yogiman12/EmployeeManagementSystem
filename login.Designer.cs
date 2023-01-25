94c923abe05fa613e380e80417d64f1d1caeee08
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-25 16:22:53 +0200

    change the dock to make it in left

--- login.Designer.cs
+++ login.Designer.cs
@@ -31,10 +31,11 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.ForeColor = System.Drawing.Color.Black;
-            this.panel1.Location = new System.Drawing.Point(128, 65);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(355, 179);
+            this.panel1.Size = new System.Drawing.Size(254, 491);
             this.panel1.TabIndex = 0;
             // 
             // login
