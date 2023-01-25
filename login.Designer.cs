f2cd64d079eacb0be7bf23b9e5b676432860e440
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-25 16:21:28 +0200

    change windows startpostion

--- login.Designer.cs
+++ login.Designer.cs
@@ -1,4 +1,3 @@
-﻿
 ﻿namespace E.M.S
 {
     partial class login
@@ -33,12 +32,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.ClientSize = new System.Drawing.Size(1193, 564);
+            this.ClientSize = new System.Drawing.Size(856, 491);
             this.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "login";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "login";
             this.ResumeLayout(false);
         }
         #endregion
