a8f628e5de3b325442b9b82247c68146543570f0
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-25 16:47:12 +0200

    add a new label

--- Form1Designer.cs
+++ Form1Designer.cs
@@ -41,6 +41,8 @@ namespace E.M.S
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -171,12 +173,32 @@ namespace E.M.S
             this.textBox5.Size = new System.Drawing.Size(292, 34);
             this.textBox5.TabIndex = 14;
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label8.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label8.Location = new System.Drawing.Point(559, 512);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(177, 28);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Employee Name";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(562, 557);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(292, 34);
+            this.textBox6.TabIndex = 16;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1412, 976);
             this.ClientSize = new System.Drawing.Size(1412, 1102);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox6);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label6);
@@ -217,5 +239,7 @@ namespace E.M.S
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox6;
     }
 }
