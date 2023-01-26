cac2686384a84b3a686512b86df3f5150c8efda7
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-26 21:51:28 +0200

    add a public function

--- function2.cs
+++ function2.cs
@@ -1,10 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 namespace EmployeeMgmt1
+{
+    public partial class Employees : Form
+    {
+        Functions Con;
+        public Employees()
+        {
+            InitializeComponent();
+            Con = new Functions();
+            ShowEmp();
+            GetDepartment();
+        }
+        private void ShowEmp()
+        {
+            try
+            {
+                string Query = "Select * from EmployeeTb1";
+                EmployeeList.DataSource = Con.GetData(Query);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
