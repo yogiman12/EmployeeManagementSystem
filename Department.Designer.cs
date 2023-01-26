ea418dd3be28b70a5b75bb4389e2278bec18517b
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-26 21:44:35 +0200

    update the department code and make it with database

--- Department.Designer.cs
+++ Department.Designer.cs
@@ -43,6 +43,7 @@ namespace E.M.S
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -95,7 +96,6 @@ namespace E.M.S
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Trebuchet MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label2.Location = new System.Drawing.Point(661, 335);
             this.label2.Location = new System.Drawing.Point(652, 243);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(267, 36);
@@ -152,7 +152,6 @@ namespace E.M.S
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(424, 149);
             this.pictureBox2.Location = new System.Drawing.Point(507, 152);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(48, 45);
@@ -166,7 +165,6 @@ namespace E.M.S
             this.label5.BackColor = System.Drawing.SystemColors.InactiveBorder;
             this.label5.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label5.Location = new System.Drawing.Point(478, 166);
             this.label5.Location = new System.Drawing.Point(561, 169);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(111, 28);
@@ -179,7 +177,6 @@ namespace E.M.S
             this.label6.BackColor = System.Drawing.SystemColors.InactiveBorder;
             this.label6.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label6.Location = new System.Drawing.Point(701, 166);
             this.label6.Location = new System.Drawing.Point(784, 169);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(135, 28);
@@ -192,7 +189,6 @@ namespace E.M.S
             this.label7.BackColor = System.Drawing.SystemColors.InactiveBorder;
             this.label7.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label7.Location = new System.Drawing.Point(945, 166);
             this.label7.Location = new System.Drawing.Point(1028, 169);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(74, 28);
@@ -205,12 +201,24 @@ namespace E.M.S
             this.label8.BackColor = System.Drawing.SystemColors.InactiveBorder;
             this.label8.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label8.Location = new System.Drawing.Point(1162, 166);
             this.label8.Location = new System.Drawing.Point(1245, 169);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(82, 28);
             this.label8.TabIndex = 48;
             this.label8.Text = "Logout";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.button3.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button3.Location = new System.Drawing.Point(127, 602);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 47);
+            this.button3.TabIndex = 49;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // Department
             // 
@@ -218,6 +226,7 @@ namespace E.M.S
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(1412, 1055);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -237,6 +246,7 @@ namespace E.M.S
             this.Name = "Department";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Department";
+            this.Text = "f";
             this.Load += new System.EventHandler(this.Department_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel1.ResumeLayout(false);
@@ -262,5 +272,6 @@ namespace E.M.S
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
     }
 }
