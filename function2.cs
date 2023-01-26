5890f41b994e5f642b737d66f352b5f7e55c190d
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-26 21:55:06 +0200

    int key = 0

--- function2.cs
+++ function2.cs
@@ -158,3 +158,22 @@ namespace EmployeeMgmt1
                                 int Salary = Convert.ToInt32(DailySalTb.Text);
                                 string Query = "Update EmployeeTb1 set EmpName = '{0}',EmpGen='{1}',EmpDep={2},EmpDOB='{3}',Empjdate='{4}',EmpSal={5} where Empid= {6}";
                                 Query = string.Format(Query, Name, Gender, Dep, DOB, JDate, Salary, key);
+                                int key = 0;
+                                private void EmployeeList_CellContentClick(object sender, DataGridViewCellEventArgs e)
+                                {
+                                    EmpNameTb.Text = EmployeeList.SelectedRows[0].Cells[1].Value.ToString();
+                                    GenCb.Text = EmployeeList.SelectedRows[0].Cells[2].Value.ToString();
+                                    DepCb.SelectedValue = EmployeeList.SelectedRows[0].Cells[3].Value.ToString();
+                                    DOBTb.Text = EmployeeList.SelectedRows[0].Cells[4].Value.ToString();
+                                    JDateTb.Text = EmployeeList.SelectedRows[0].Cells[5].Value.ToString();
+                                    DailySalTb.Text = EmployeeList.SelectedRows[0].Cells[6].Value.ToString();
+                                    if (EmpNameTb.Text == "")
+                                    {
+                                        key = 0;
+                                    }
+                                    else
+                                    {
+                                        key = Convert.ToInt32(EmployeeList.SelectedRows[0].Cells[0].Value.ToString());
+                                    }
+                                }
+                            }
