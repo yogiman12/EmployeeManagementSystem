7c63588aebda8fd72a9c397daa1b36b8345d232d
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-26 22:00:42 +0200

    add classes for salary window

--- function3.cs
+++ function3.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace EmployeeManagement
+using System.Windows.Forms;
+namespace EmployeeMgmt1
 {
-    internal class function3
-    {
-    }
-}
