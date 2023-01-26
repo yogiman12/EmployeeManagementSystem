31ac59c8ef816286391b7833cc256c3ca8fbb6fc
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-26 22:06:39 +0200

    add string name

--- function3.cs
+++ function3.cs
@@ -52,7 +52,7 @@ namespace EmployeeMgmt1
         private void Employees_Load(object sender, EventArgs e)
         {
         }
-         {
+        {
             string Query = "Select * from DepartmentTb1";
         DepCb.DisplayMember = Con.GetData(Query).Columns["Depname"].ToString();
         DepCb.ValueMember = Con.GetData(Query).Columns["Depid"].ToString();
@@ -68,3 +68,20 @@ namespace EmployeeMgmt1
             }
             else
             {
+                string Name = EmpNameTb.Text;
+                string Gender = GenCb.SelectedItem.ToString();
+                int Dep = Convert.ToInt32(DepCb.SelectedValue.ToString());
+                string DOB = DOBTb.Value.ToString();
+                string JDate = JDateTb.Value.ToString();
+                int Salary = Convert.ToInt32(DailySalTb.Text);
+                string Query = "insert into EmployeeTb1 values('{0}','{1}',{2},'{3}','{4}',{5})";
+                Query = string.Format(Query, Name, Gender, Dep, DOB, JDate, Salary);
+                Con.SetData(Query);
+                ShowEmp();
+                MessageBox.Show("Emoloyee Updated!!!");
+                EmpNameTb.Text = "";
+                DailySalTb.Text = "";
+                GenCb.SelectedIndex = -1;
+                DepCb.SelectedIndex = -1;
+            }
+        }
