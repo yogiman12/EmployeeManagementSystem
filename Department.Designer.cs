b79c1ae677329182bbfc2331fd9442b8311a08d5
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-26 21:31:34 +0200

    add a new labels in department window

--- Department.Designer.cs
+++ Department.Designer.cs
@@ -151,10 +151,8 @@ namespace E.M.S
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(421, 149);
             this.pictureBox2.Location = new System.Drawing.Point(424, 149);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(72, 45);
             this.pictureBox2.Size = new System.Drawing.Size(48, 45);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 44;
@@ -166,7 +164,6 @@ namespace E.M.S
             this.label5.BackColor = System.Drawing.SystemColors.InactiveBorder;
             this.label5.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label5.Location = new System.Drawing.Point(499, 166);
             this.label5.Location = new System.Drawing.Point(478, 166);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(111, 28);
@@ -204,6 +201,7 @@ namespace E.M.S
             this.label8.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.SystemColors.Highlight;
             this.label8.Location = new System.Drawing.Point(639, 513);
+            this.label8.Location = new System.Drawing.Point(1162, 166);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(82, 28);
             this.label8.TabIndex = 48;
