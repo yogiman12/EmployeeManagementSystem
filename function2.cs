5a8edcde3c9acd35718230322be9d6ea042eab48
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-26 21:52:14 +0200

    add private viod for labels

--- function2.cs
+++ function2.cs
@@ -1,6 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace EmployeeMgmt1
 {
     public partial class Employees : Form
@@ -25,6 +31,14 @@ namespace EmployeeMgmt1
                 throw;
             }
         }
-
         private void textBox1_TextChanged(object sender, EventArgs e)
+        { }
+        private void label3_Click(object sender, EventArgs e)
         {
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+        }
