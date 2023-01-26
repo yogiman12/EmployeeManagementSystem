eff654ba32715cf1f01bb065e314c0c129b15911
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-26 21:34:36 +0200

    edit the second table in database

--- Salary.Designer.cs
+++ Salary.Designer.cs
@@ -260,7 +260,6 @@
             this.label8.BackColor = System.Drawing.SystemColors.InactiveBorder;
             this.label8.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label8.Location = new System.Drawing.Point(1288, 144);
             this.label8.Location = new System.Drawing.Point(1278, 144);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(82, 28);
@@ -274,6 +273,7 @@
             this.label7.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.SystemColors.Highlight;
             this.label7.Location = new System.Drawing.Point(1071, 144);
+            this.label7.Location = new System.Drawing.Point(1087, 144);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(74, 28);
             this.label7.TabIndex = 52;
