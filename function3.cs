c091940d71ee325df6311004b0ebb0332a32ba5c
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-26 22:10:08 +0200

    add database

--- function3.cs
+++ function3.cs
@@ -150,3 +150,11 @@ namespace EmployeeMgmt1
                         }
                         else
                         {
+                            string Name = EmpNameTb.Text;
+                            string Gender = GenCb.SelectedItem.ToString();
+                            int Dep = Convert.ToInt32(DepCb.SelectedValue.ToString());
+                            string DOB = DOBTb.Value.Date.ToString();
+                            string JDate = JDateTb.Value.Date.ToString();
+                            int Salary = Convert.ToInt32(DailySalTb.Text);
+                            string Query = "Update EmployeeTb1 set EmpName = '{0}',EmpGen='{1}',EmpDep={2},EmpDOB='{3}',Empjdate='{4}',EmpSal={5} where Empid= {6}";
+                            Query = string.Format(Query, Name, Gender, Dep, DOB, JDate, Salary, key);
