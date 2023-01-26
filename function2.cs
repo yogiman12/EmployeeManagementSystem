45da741991c9b6cf089fd4506de1540eb16fffab
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-26 21:53:58 +0200

    private viod update

--- function2.cs
+++ function2.cs
@@ -122,3 +122,21 @@ namespace EmployeeMgmt1
                 MessageBox.Show(Ex.Message);
             }
         }
+        private void UpdateBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (EmpNameTb.Text == "" || GenCb.SelectedIndex == -1 || DepCb.SelectedIndex == -1 || DailySalTb.Text == "")
+                {
+                    MessageBox.Show("missing data!!!");
+                }
+                else
+                {
+                    string Name = EmpNameTb.Text;
+                    string Gender = GenCb.SelectedItem.ToString();
+                    int Dep = Convert.ToInt32(DepCb.SelectedValue.ToString());
+                    string DOB = DOBTb.Value.Date.ToString();
+                    string JDate = JDateTb.Value.Date.ToString();
+                    int Salary = Convert.ToInt32(DailySalTb.Text);
+                    string Query = "Update EmployeeTb1 set EmpName = '{0}',EmpGen='{1}',EmpDep={2},EmpDOB='{3}',Empjdate='{4}',EmpSal={5} where Empid= {6}";
+                    Query = string.Format(Query, Name, Gender, Dep, DOB, JDate, Salary, key);
