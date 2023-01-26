13649f65e19649c351c823c71a71b9e7d390f62f
Ahmed Mostafa Kamel <yogiahmed12@gmail.com>
2023-01-26 22:20:25 +0200

    private viod button 1click

--- function4.cs
+++ function4.cs
@@ -1,11 +1,48 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace EmployeeManagement
+using System.Windows.Forms;
+namespace EmployeeMgmt1
 {
-    internal class function4
+    public partial class login : Form
     {
-    }
-}
+        public login()
+        {
+            InitializeComponent();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+        private void login_Load(object sender, EventArgs e)
+        {
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (UNameTb.Text == "" || PasswordTb.Text == "")
+            {
+                MessageBox.Show("Missing Data!!!");
+            }
+            else if (UNameTb.Text == "Admin" && PasswordTb.Text == "Password")
+            {
+                Employees Obj = new Employees();
+                Obj.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("wrong user name or password!!!");
+                UNameTb.Text = "";
+                PasswordTb.Text = "";
+            }
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
